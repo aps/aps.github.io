b2ecbd16df045139428561b3913950e10f9df47d
Arne Poths <mail@arnepoths.de>
2016-02-25 16:29:44 +0100

    update email data

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,12 +19,11 @@ if ($email === FALSE) {
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
-
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'mail@arnepoths.de';
+$email_subject = "Kontaktformular:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@arnepoths.de\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
